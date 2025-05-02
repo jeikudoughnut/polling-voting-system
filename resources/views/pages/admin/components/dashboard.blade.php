b3a94c2fc23f65d812bf5724d46822cdda20149a
jeikudoughnut <jakeruzzelc@gmail.com>
2025-05-02 15:09:52 +0800

    insert 2 images

--- resources/views/pages/admin/components/dashboard.blade.php
+++ resources/views/pages/admin/components/dashboard.blade.php
@@ -1,8 +1,8 @@
 <div class="grid grid-row-2 gap-y-4">
 
     <div class="div bg-white h-[500px] rounded-lg border-2 border-gray-300">
+    <img src="https://media.npr.org/assets/img/2015/09/23/ap_836720500193-d90a20e2b8d735f74d436f36054eb3dc2bd96696.jpg?s=800&c=85&f=webp" alt="Random Image" class="w-full h-full object-cover rounded-lg">
     </div>
-
     <div class="div grid grid-cols-3 h-[400px] gap-x-4">
 
             <div class="bg-white rounded-lg col-span-2 border-2 border-gray-300">
@@ -11,7 +11,7 @@
             <div class="bg-white rounded-lg border-2 border-gray-300">
 
             </div>
-
+            <img src="https://media.npr.org/assets/img/2015/09/23/ap_836720500193-d90a20e2b8d735f74d436f36054eb3dc2bd96696.jpg?s=800&c=85&f=webp" alt="Random Image" class="w-full h-full object-cover rounded-lg">
     </div>
 
 </div>
